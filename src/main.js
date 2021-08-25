9e419b8e7d760cfc4d9baebf8f95385091ae166d
sarmai <ailensarmukhanova@gmail.com>
2021-08-25 14:58:07 +0600

    basic setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/styles/normalize.css'
+import './assets/styles/colors.css'
+import './assets/styles/styles.css'
+import router from './router'
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
